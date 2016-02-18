f0e5aff01cc1af7957d5c67bdc29b87fe0fd8da9
Hallison Batista <hallisonbatista@gmail.com>
2016-02-18 15:01:49 -0400

    Teste do modelo usuário
    - Validação dos dados para salvar uma nova conta.
    - Ajustes do teste de usuário.

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -1,12 +1,21 @@
 describe 'Usuário' do
+  before do
+    Prodam::Idealize::Usuario.where(nome_usuario: 'p001051').delete
+  end
+
   it 'cria uma nova conta' do
     usuario = Prodam::Idealize::Usuario.new nome_usuario: 'p001051', nome: 'Hallison Batista', email: 'hallison@localhost.local'
     assert usuario.valid?
     assert_equal 0, usuario.errors.size
+
+    usuario.save senha: 's3kr3t4'
+
+    usuario = Prodam::Idealize::Usuario.find nome_usuario: 'p001051'
+    assert usuario
   end
 
   it 'consulta conta uma existente' do
     usuario = Prodam::Idealize::Usuario.find nome_usuario: 'admin'
-    refute_nil usuario
+    assert usuario
   end
 end
