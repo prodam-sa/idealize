d5e32f6f906e2c200137faa35f0aed4e192f5823
Hallison Batista <hallisonbatista@gmail.com>
2016-02-19 11:15:04 -0400

    Simplificação do controller principal da aplicação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,29 +2,7 @@
 
 require 'sinatra/base'
 
-class Prodam::Idealize::Application < Sinatra::Base
-  class << self
-    def load_config(file, env = :development)
-      @config = Prodam::Idealize::Configuration.load_file(file)
-    end
-
-    def routes
-      controllers.each_with_object({}) do |(id, controller), routes|
-        routes[controller[:url_path]] = Prodam::Idealize.const_get controller[:const_name]
-      end
-    end
-
-    def controllers
-      @controllers ||= config[:application][:routes].each_with_object({}) do |(path, data), controller|
-        id = data[:controller].underscore.to_sym
-        data[:require_path] = format('%s/%s', :controllers, id)
-        data[:url_path] = path
-        data[:const_name] = data[:controller].to_sym
-        controller[id] = data
-      end
-    end
-  end
-
+class Prodam::Idealize::ApplicationController < Sinatra::Base
   set :public_folder, 'public'
   set :views, 'app/views'
   set :auth do |*any|
