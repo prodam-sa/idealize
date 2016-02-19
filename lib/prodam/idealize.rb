c4d2aefe3c3a6c5ea22fa2770c57a1f63526ad6b
Hallison Batista <hallisonbatista@gmail.com>
2016-02-19 11:11:53 -0400

    Simplificação das classes de configuração
    - As classes de configurações de aplicação e banco de dados foram
      simplificadas.
    - Remoção dos testes de configuração

--- lib/prodam/idealize.rb
+++ lib/prodam/idealize.rb
@@ -19,17 +19,80 @@ end
 
 module Prodam
   module Idealize
-    # Base
-    autoload :Configuration, 'prodam/idealize/configuration'
-    autoload :Database, 'prodam/idealize/database'
-    autoload :Application, 'prodam/idealize/application'
+
+    class << self
+      def root_directory
+        @root_directory ||= Pathname.new(File.expand_path("#{File.basename(__FILE__)}/.."))
+      end
+
+      def environment
+        (ENV['RACK_ENV'].nil? ? :development : ENV['RACK_ENV']).to_sym
+      end
+
+      def load_config(file)
+        YAML.load_file(root_directory.join('config').join("#{file}.yml"))
+      end
+
+      def application_config
+        @application_config ||= load_config(:application)
+      end
+
+      def database_config
+        @database_config ||= load_config(:database)
+      end
+
+      def routes
+        controllers.each_with_object({}) do |(id, controller), routes|
+          routes[controller[:url_path]] = Prodam::Idealize.const_get controller[:const_name]
+        end
+      end
+
+      def controllers
+        @controllers ||= application_config[:routes].each_with_object({}) do |(path, data), controller|
+          id = data[:controller].underscore.to_sym
+          data[:require_path] = format('%s/%s', :controllers, id)
+          data[:url_path] = path
+          data[:const_name] = data[:controller].to_sym
+          controller[id] = data
+        end
+      end
+    end
+
+    class Prodam::Idealize::Database
+      class << self
+        attr_reader :uri
+        attr_reader :options
+
+        def connection(env = Prodam::Idealize.environment)
+          @uri = Prodam::Idealize.database_config[env.to_sym][:uri]
+          @options = { prefetch_rows: 50 }
+          if env == :development
+            require 'logger'
+            @options[:loggers] = [Logger.new($stdout)]
+          end
+          @connection ||= Sequel.connect @uri, @options
+        end
+
+        def [](dataset)
+          connection[dataset]
+        end
+      end
+    end
+
+    class Prodam::Idealize::Model
+      def self.[](dataset)
+        Sequel::Model(Database[dataset])
+      end
+    end
 
     # Models
     autoload :Usuario, 'models/usuario'
 
-    def self.initialize(env = :development)
-      Database.load_config('config/database.yml', env)
-      Application.load_config('config/application.yml', env)
+    # Controllers
+    autoload :Application, 'controllers/application_controller'
+
+    controllers.each do |id, controller|
+      autoload controller[:const_name], controller[:require_path]
     end
   end
 end
--- lib/prodam/idealize/configuration.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-class Prodam::Idealize::Configuration #:nodoc:
-  def self.load_file(file)
-    YAML.load_file(file)
-  end
-end
