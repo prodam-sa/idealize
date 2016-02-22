e0ffd8bad323f57035685c42902034b2dd529e65
Hallison Batista <hallisonbatista@gmail.com>
2016-02-22 11:01:50 -0400

    Correção das informações sobre versionamento

--- lib/prodam/idealize/version.rb
+++ lib/prodam/idealize/version.rb
@@ -1,19 +1,21 @@
 # encoding: utf-8
 
-module Procon
-  VERSION   = '0.1.0'
-  RELEASE   = ''
-  TIMESTAMP = '2016-01-13 09:19:08 -0400'
+module Prodam
+  module Idealize
+    VERSION   = '0.1.0'
+    RELEASE   = Date.today
+    TIMESTAMP = '2016-01-13 09:19:08 -0400'
 
-  def self.info
-    "#{name} v#{VERSION} (#{RELEASE})"
-  end
+    def self.info
+      "#{Prodam.constants.first} v#{VERSION} (#{RELEASE})"
+    end
 
-  def self.to_h
-    { :name      => name,
-      :version   => VERSION,
-      :semver    => VERSION.to_semver_h,
-      :release   => RELEASE,
-      :timestamp => TIMESTAMP }
+    def self.to_h
+      { :name      => name,
+        :version   => VERSION,
+        :semver    => VERSION.to_semver_h,
+        :release   => RELEASE,
+        :timestamp => TIMESTAMP }
+    end
   end
 end
