af6922e82d9af9ccf13919ac6b2f2fa7f7e1e1c0
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-03-21 15:31:12 -0400

    Controller e views de ideias

---
+++ app/controllers/ideias_controller.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+class Prodam::Idealize::IdeiasController < Prodam::Idealize::ApplicationController
+  helpers Prodam::Idealize::IdeiasHelper
+
+  before do
+    @page = controllers[:ideias_controller]
+  end
+
+  get '/nova', authenticate: true do
+    @ideia = Prodam::Idealize::Ideia.new
+    @categorias = Prodam::Idealize::Categoria.all
+    'TODO: Formulário de ideia'
+  end
+
+  post '/' do
+    params[:ideia].to_s
+  end
+
+  get '/' do
+    @ideias = Prodam::Idealize::Ideia.latest(5)
+    @ideia = Prodam::Idealize::Ideia.new
+    view 'ideias/index'
+  end
+  
+  get '/:id' do |id|
+    @ideia = Prodam::Idealize::Ideia[id.to_i]
+    view 'ideias/page'
+  end
+
+  get '/autor/:autor_id' do |autor_id|
+    @ideias = Prodam::Idealize::Ideia.all_by_autor(autor_id.to_i)
+    @ideias.to_s
+  end
+end
