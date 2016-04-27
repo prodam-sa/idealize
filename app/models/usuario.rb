0f0d7d1390b970413bb8da0b7f3f38426a4de7c0
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-04-27 17:07:15 -0400

    Permissão para o campo de perfil moderador

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -6,7 +6,7 @@ class Prodam::Idealize::Usuario < Prodam::Idealize::Model[:usuario]
   include Prodam::Idealize::Model
 
   plugin :validation_helpers
-  set_allowed_columns :nome_usuario, :nome, :email, :ad
+  set_allowed_columns :nome_usuario, :nome, :email, :ad, :mi
 
   def validate
     super
