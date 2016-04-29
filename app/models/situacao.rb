a60d5d3c756ac0765e980c899d9ee9f406a80469
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-04-29 14:25:23 -0400

    Classe modelo para situação de uma ideia

---
+++ app/models/situacao.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+class Prodam::Idealize::Situacao < Prodam::Idealize::Model[:situacao]
+  include Prodam::Idealize::Model
+
+  plugin :validation_helpers
+
+  def validate
+    super
+    validates_presence :titulo, message: 'não foi atribuído.'
+    validates_presence :descricao, message: 'deve ser preenchido.'
+    validates_unique :titulo, message: 'já registrado.'
+  end
+
+  def param_name
+    "#{id}-#{titulo.downcase.tr(' ', '-')}"
+  end
+end
