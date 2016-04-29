f82fb3beed6c9102ef2707c684f6cbdbfe07238c
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-04-29 16:41:58 -0400

    Atualização do SQL de migração v0.2.0

--- db/v0.2.0/create.sql
+++ db/v0.2.0/create.sql
@@ -2,3 +2,5 @@ define indx = &1
 define data = &2
 
 @@db/v0.2.0/create/USUARIO.sql &indx &data
+@@db/v0.2.0/create/FORMULARIO.sql &indx &data
+@@db/v0.2.0/create/CRITERIO.sql &indx &data
--- db/v0.2.0/drop.sql
+++ db/v0.2.0/drop.sql
@@ -1,1 +1,3 @@
+@@db/v0.2.0/drop/CRITERIO.sql
+@@db/v0.2.0/drop/FORMULARIO.sql
 @@db/v0.2.0/drop/USUARIO.sql
