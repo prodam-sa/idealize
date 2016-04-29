5209e5fd7a3f9f6e71ed5b084e5bd7b20d87ac19
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-04-29 17:04:28 -0400

    Inicialização do banco de dados v0.2.0

---
+++ db/v0.2.0/bootstrap.rb
@@ -0,0 +1,19 @@
+require 'prodam/idealize'
+
+include Prodam::Idealize
+
+yaml = YAML.load_file('db/v0.2.0/bootstrap.yml')
+
+yaml[:formularios].each do |data|
+  formulario = Formulario.new(titulo: data[:titulo], descricao: data[:descricao])
+  formulario.save
+  if formulario.errors.empty?
+    data[:criterios].each do |item|
+      criterio = Criterio.new(titulo: item[:titulo], descricao: item[:descricao])
+      criterio.formulario_id = formulario.id
+      criterio.save
+    end
+  else
+    printf("- %-32s %s", formulario.titulo, :error)
+  end
+end
