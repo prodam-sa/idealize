61ea7d275fb67f1a69edd9857c71a366036312e3
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-04-30 12:59:55 -0400

    Melhoria no código do controller de ideias

--- app/controllers/ideias_controller.rb
+++ app/controllers/ideias_controller.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
-class Prodam::Idealize::IdeiasController < Prodam::Idealize::ApplicationController
-  helpers Prodam::Idealize::IdeiasHelper
+module Prodam::Idealize
+
+class IdeiasController < ApplicationController
+  helpers IdeiasHelper
 
   before do
     @page = controllers[:ideias_controller]
@@ -9,25 +11,25 @@ class Prodam::Idealize::IdeiasController < Prodam::Idealize::ApplicationControll
 
   before '/:id/?:action?' do |id, action|
     if (ideia_id = id.to_i) > 0
-      @ideia = Prodam::Idealize::Ideia[ideia_id]
+      @ideia = Ideia[ideia_id]
     else
-      @ideia = Prodam::Idealize::Ideia.new
+      @ideia = Ideia.new
     end
   end
 
   get '/' do
     ideias_list
-    @ideia = Prodam::Idealize::Ideia.new
+    @ideia = Ideia.new
     view 'ideias/index'
   end
   
   get '/nova', authenticate: true do
-    @categorias = Prodam::Idealize::Categoria.all
+    @categorias = Categoria.all
     view 'ideias/form'
   end
 
   post '/', authenticate: true do
-    @ideia = Prodam::Idealize::Ideia.new(params[:ideia])
+    @ideia = Ideia.new(params[:ideia])
     @ideia.autor_id = usuario_id
     @ideia.save
     view 'ideias/page'
@@ -38,7 +40,7 @@ class Prodam::Idealize::IdeiasController < Prodam::Idealize::ApplicationControll
   end
 
   get '/:id/editar' do |id|
-    @categorias = Prodam::Idealize::Categoria.all
+    @categorias = Categoria.all
     view 'ideias/form'
   end
 
@@ -63,7 +65,7 @@ class Prodam::Idealize::IdeiasController < Prodam::Idealize::ApplicationControll
   end
 
   get '/autor/:autor_id' do |autor_id|
-    @ideias = Prodam::Idealize::Ideia.all_by_autor(autor_id.to_i)
+    @ideias = Ideia.all_by_autor(autor_id.to_i)
     @ideias.to_s
   end
 
@@ -71,11 +73,13 @@ private
 
   def ideias_list
     @ideias = {
-      latest: Prodam::Idealize::Ideia.latest(5),
+      latest: Ideia.latest(5),
       all_by_autor: nil
     }
-    if usuario_id && autor = Prodam::Idealize::Autor[usuario_id]
-      @ideias[:all_by_autor] = Prodam::Idealize::Ideia.all_by_autor(autor.id)
+    if usuario_id && autor = Autor[usuario_id]
+      @ideias[:all_by_autor] = Ideia.all_by_autor(autor.id)
     end
   end
 end
+
+end # module
