96bdad468853594edc45c60197ed97b504d5e9cb
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-05-09 13:25:32 -0400

    Configurações do servidor Puma e Nginx
    - Servidor Puma será executado através de socket (unix://).
    - O servidor Nginx servirá de proxy para upstream do socket Puma.
    - Modificações finalizam #17.

---
+++ config/puma.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env puma
+
+# directory '/u/apps/lolcat'
+
+@environment = ENV['RACK_ENV'] || 'development'
+
+environment @environment
+
+daemonize true
+
+pidfile "tmp/idealize-#{@environment}.pid"
+
+state_path "tmp/idealize-#{@environment}.state"
+
+threads 0, 16
+
+bind "unix:///tmp/idealize-#{@environment}.sock"
+
+on_restart do
+  puts 'On restart...'
+end
+
+workers 0
+
+tag 'idealize'
