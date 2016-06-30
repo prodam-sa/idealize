9b1a9fb238f70cf5eae97b221e87a719988292d1
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-06-30 14:58:05 -0400

    Validação dos critérios

--- app/models/criterio.rb
+++ app/models/criterio.rb
@@ -14,9 +14,12 @@ class Criterio < Model[:criterio]
 
   def validate
     super
-    validates_presence :titulo, message: 'não foi atribuído.'
-    validates_presence :descricao, message: 'deve ser preenchido.'
-    validates_unique :titulo, message: 'já registrado.'
+    validates_presence :titulo, message: 'deve ser atribuído'
+    validates_unique [:titulo, :criterio_multiplo_id], message: 'já registrado'
+    validates_max_length 64, :titulo, message: lambda{ |n| "deve ser de até #{n} caracteres" }
+
+    validates_presence :descricao, message: 'deve ser preenchido'
+    validates_max_length 256, :descricao, message: lambda{ |n| "deve ser de até #{n} caracteres" }
   end
 
   def param_name
