e4338366031d4d0d4c4160a8bc0096c906f1874d
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-07-01 14:47:56 -0400

    Atualização da migração SQL v0.12.0

--- db/v0.12.0/create.sql
+++ db/v0.12.0/create.sql
@@ -3,3 +3,4 @@ define data = &2
 
 @@db/v0.12.0/create/CRITERIO.sql &indx &data
 @@db/v0.12.0/create/FORMULARIO.sql &indx &data
+@@db/v0.12.0/create/IDEIA_APOIADOR.sql &indx &data
--- db/v0.12.0/drop.sql
+++ db/v0.12.0/drop.sql
@@ -1,2 +1,3 @@
+@@db/v0.12.0/drop/IDEIA_APOIADOR.sql
 @@db/v0.12.0/drop/FORMULARIO.sql
 @@db/v0.12.0/drop/CRITERIO.sql
