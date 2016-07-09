92616807623d7e62caa8ef874b58833c09e5ff79
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-07-09 16:24:43 -0400

    Modificação do controller de ideias
    - O controller será utilizado apenas para o acesso público das ideias.
      Todas as ações de postagem, moderação e avaliação serão gerenciadas
      por seus respectivos controllers.

--- app/controllers/ideias_controller.rb
+++ app/controllers/ideias_controller.rb
@@ -22,12 +22,9 @@ class IdeiasController < ApplicationController
   end
 
   get '/' do
-    ideias_list
+    @relatorio = Relatorio.new(ideias: Ideia.find_by_situacoes(@situacoes).order(:data_criacao, :data_publicacao).all)
+    @ideias = @relatorio.ideias
     view 'ideias/index'
-  end
-  
-  get '/nova', authenticate: true do
-    view 'ideias/form'
   end
 
   get '/pesquisa' do
@@ -36,34 +33,7 @@ class IdeiasController < ApplicationController
     view 'ideias/search'
   end
 
-  post '/', authenticate: true do
-    @situacao = situacao(:rascunho)
-    @ideia = Ideia.new(params[:ideia])
-    @ideia.autor_id = usuario_id
-
-    if @ideia.valid?
-      @ideia.save
-
-      if params[:postar]
-        @situacao = situacao(:postagem)
-        historico(@ideia, @situacao, mensagem('Ideia postada pelo autor para moderação.')).save
-        message.update(level: :information, text: 'Sua ideia foi postada com sucesso!')
-      else
-        historico(@ideia, @situacao, mensagem('Ideia criada em rascunho para edição.')).save
-        message.update(level: :information, text: 'Sua ideia foi registrada em rascunho. Não esqueça de postar depois de finalizar.')
-      end
-
-      @ideia.situacao = @situacao.chave
-      @ideia.save
-      redirect to("/#{@ideia.to_url_param}")
-    else
-      message.update(level: :error, text: 'Oops! Tem alguma coisa errada. Observe os campos em vermelho.')
-      view 'ideias/form'
-    end
-  end
-
   get '/:id' do |id|
-    @categorias = Categoria.all if permitido_moderar? @ideia
     @apoiadores = Relatorio.new.lista_apoiadores_ideia @ideia
     if (@ideia.publicada?) or (usuario_autor? @ideia) or (authorized_by? :moderador)
       view 'ideias/page'
@@ -71,47 +41,6 @@ class IdeiasController < ApplicationController
       message.update(level: :error, text: 'Você só poderá ler essa ideia depois de sua publicação.')
       redirect to('/')
     end
-  end
-
-  get '/:id/editar' do |id|
-    unless @ideia.bloqueada?
-      view 'ideias/form'
-    else
-      if @ideia.publicada?
-        mensagem = 'Sua ideia já foi publicada.'
-      else
-        mensagem = 'Sua ideia está em moderação. Por enquanto, ela não poderá ser alterada.'
-      end
-      message.update(level: :warning, text: mensagem)
-      redirect to(id)
-    end
-  end
-
-  put '/:id' do |id|
-    unless @ideia.bloqueada?
-      @ideia.set_all(params[:ideia])
-      if @ideia.valid?
-        @ideia.save
-        historico(@ideia, situacao(:revisao), mensagem('Ideia revisada pelo autor')).save
-        message.update(level: :information, text: 'Sua ideia foi atualizada com sucesso!')
-        redirect to(id)
-      else
-        message.update(level: :error, text: 'Oops! Tem alguma coisa errada. Observe os campos em vermelho.')
-        @categorias = Categoria.all
-        view 'ideias/form'
-      end
-    else
-      message.update(level: :warning, text: 'Sua ideia ainda está em moderação.')
-      redirect to(id)
-    end
-  end
-
-  put '/:id/postar' do |id|
-    @ideia.situacao = :postagem
-    @ideia.save
-    historico(@ideia, situacao(:postagem), mensagem('Ideia postada pelo autor para moderação.')).save
-    message.update(level: :information, text: 'Sua ideia foi postada com sucesso!')
-    redirect to("/#{@ideia.to_url_param}")
   end
 
   put '/:id/apoiar' do |id|
@@ -127,48 +56,6 @@ class IdeiasController < ApplicationController
       message.update(level: :information, text: 'Seu apoio foi registrado.')
     else
       message.update(level: :error, text: 'Infelizmente, você não poderá apoiar essa ideia.')
-    end
-    redirect to(id)
-  end
-
-  delete '/:id', authenticate: true  do |id|
-    unless @ideia.bloqueada?
-      @ideia.remove_all_coautores
-      @ideia.remove_all_categorias
-      @ideia.remove_all_modificacoes
-      @ideia.delete
-      ideias_list
-      message.update(level: :information, text: 'Sua ideia foi excluída.')
-      redirect to('/')
-    else
-      message.update(level: :warning, text: 'Sua ideia está em moderação e não poderá ser excluída.')
-      redirect to(id)
-    end
-  end
-
-  put '/:id/coautores', authenticate: true do |id|
-    if @ideia.desbloqueada? && (usuario_autor? @ideia)
-      @ideia.remove_all_coautores
-      params[:coautores] && params[:coautores].each do |coautor_id|
-        @ideia.add_coautor coautor_id
-      end
-      @ideia.remove_coautor usuario_id if (@ideia.coautores.include? usuario_id)
-      message.update(level: :information, text: 'Os coautores de sua ideia foram atualizados.')
-    else
-      message.update(level: :warning, text: 'Sua ideia está bloqueada para inclusão de coautores.')
-    end
-    redirect to(id)
-  end
-
-  put '/:id/categorias', authenticate: true do |id|
-    if @ideia.desbloqueada? && (permitido_moderar? @ideia)
-      @ideia.remove_all_categorias
-      params[:categorias] && params[:categorias].each do |categoria_id|
-        @ideia.add_categoria categoria_id
-      end
-      message.update(level: :information, text: 'As categorias foram atualizadas.')
-    else
-      message.update(level: :warning, text: 'A ideia está bloqueada para alteração de categorias.')
     end
     redirect to(id)
   end
@@ -259,14 +146,6 @@ private
     end
 
     check.size == params[:criterios].size
-  end
-
-  def ideias_list
-    @relatorio = {
-      publicacoes: Relatorio.new(ideias: Ideia.find_by_situacoes(@situacoes).order(:data_criacao, :data_publicacao).all),
-      moderacao: Relatorio.new(ideias: Ideia.find_by_situacao(['postagem', 'moderacao']).exclude(autor_id: usuario_id).order(:data_criacao, :data_publicacao).all),
-      rascunhos: Relatorio.new(ideias: Ideia.find_by_autor(usuario_id).order(:data_criacao).all),
-    }
   end
 end
 
