56dc17f5e04460d45c66ae2ac12579a237886cd9
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-07-09 18:21:38 -0400

    Arquivos de migração do banco de dados v0.13.0

---
+++ db/v0.13.0/create.sql
@@ -0,0 +1,5 @@
+define indx = &1
+define data = &2
+
+@@db/v0.13.0/create/CLASSIFICACAO.sql &indx &data
+@@db/v0.13.0/create/AVALIACAO.sql &indx &data
---
+++ db/v0.13.0/drop.sql
@@ -0,0 +1,2 @@
+@@db/v0.13.0/drop/AVALIACAO.sql
+@@db/v0.13.0/drop/CLASSIFICACAO.sql
