2c16773d67b3c2176d44220674cc586a0192884e
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-08-02 10:31:23 -0400

    Helper para envio de emails

---
+++ app/helpers/mail_helper.rb
@@ -0,0 +1,27 @@
+require 'mail'
+
+# Mail.defaults do
+#   delivery_method :smtp, address: "localhost", port: 1025
+# end
+
+module Prodam::Idealize
+
+module Mail
+  def enviar_mensagem(options)
+    options[:de] ||= 'nao-responda@prodam.am.gov.br'
+    mail = Mail.new do
+      from     options[:de]
+      to       options[:para]
+      subject  options[:assunto]
+      body     options[:texto]
+      options[:arquivos].each do |arquivo|
+        add_file filename: arquivo[:nome],
+                 content:  arquivos[:conteudo]
+      end
+    end
+    # mail.delivery_method :sendmail
+    mail.deliver!
+  end
+end
+
+end # Prodam::Idealize
