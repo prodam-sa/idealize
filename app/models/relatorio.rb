26681be40fc65a78138e7f6e110b2bb01727a45e
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-12-13 15:36:59 -0400

    Métodos para totalizadores na classe de relatório

--- app/models/relatorio.rb
+++ app/models/relatorio.rb
@@ -4,10 +4,17 @@ module Prodam::Idealize
 
 class Relatorio
   SQL = {
+    total_ideias_por_data_postagem: 'SELECT TRUNC(data_criacao) AS data_criacao, COUNT(*) AS total FROM ideia GROUP BY TRUNC(data_criacao)',
     total_coautores_por_ideia: 'SELECT ideia_id, COUNT(coautor_id) AS total FROM ideia_coautor GROUP BY ideia_id',
-    total_apoiadores_por_ideia: 'SELECT ideia_id, COUNT(apoiador_id) AS total FROM ideia_apoiador GROUP BY ideia_id'
+    total_apoiadores_por_ideia: 'SELECT ideia_id, COUNT(apoiador_id) AS total FROM ideia_apoiador GROUP BY ideia_id',
+    total_ideias_por_categoria: 'SELECT categoria_id, COUNT(ideia_id) AS total FROM  ideia_categoria GROUP BY categoria_id',
+    total_ideias_sem_categoria: 'SELECT COUNT(id) AS total FROM ideia WHERE id NOT IN (SELECT DISTINCT ideia_id FROM ideia_categoria)',
+    total_ideias_por_situacao: 'SELECT s.id AS situacao_id, COUNT(i.id) AS total FROM ideia i INNER JOIN situacao s ON (s.chave = i.situacao) GROUP BY s.id',
+    total_ideias_por_autor: 'SELECT a.id AS autor_id, i.id AS ideia_id, COUNT(i.id) AS total FROM usuario a INNER JOIN ideia i ON (i.autor_id = a.id) GROUP BY a.id ORDER BY COUNT(i.id)',
+    total_ideias_por_autor_situacao: 'SELECT a.id AS autor_id, s.id AS situacao_id, COUNT(i.id) AS total FROM usuario a INNER JOIN ideia i ON (i.autor_id - a.id) GROUP BY a.id',
   }.freeze
 
+  attr_accessor :autor
   attr_accessor :ideia
   attr_accessor :ideias
 
@@ -97,6 +104,66 @@ class Relatorio
       @lista_ideias_apoiadores[row[:ideia_id]] << row[:apoiador_id]
     end
     @lista_ideias_apoiadores
+  end
+
+  # Categorias
+  def total_ideias_por_categoria
+    @total_ideias_por_categoria || total_ideias_por_categoria!
+  end
+
+  def total_ideias_por_categoria!
+    @total_ideias_por_categoria = {}
+    Database[SQL[:total_ideias_por_categoria]].all.map do |row|
+      @total_ideias_por_categoria[row[:categoria_id]] = row[:total].to_i
+    end
+    @total_ideias_por_categoria
+  end
+
+  def total_ideias_sem_categoria
+    @total_ideias_sem_categoria || total_ideias_sem_categoria!
+  end
+
+  def total_ideias_sem_categoria!
+    @total_ideias_sem_categoria = Database[SQL[:total_ideias_sem_categoria]].first[:total].to_i
+  end
+
+  # Situações
+  def total_ideias_por_situacao
+    @total_ideias_por_situacao || total_ideias_por_situacao!
+  end
+
+  def total_ideias_por_situacao!
+    @total_ideias_por_situacao = {}
+    Database[SQL[:total_ideias_por_situacao]].all.map do |row|
+      @total_ideias_por_situacao[row[:situacao_id]] = row[:total].to_i
+    end
+    @total_ideias_por_situacao
+  end
+
+  # Ideias por autor
+  def total_ideias_por_autor
+    @total_ideias_por_situacao || total_ideias_por_situacao!
+  end
+
+  def total_ideias_por_autor!
+    @total_ideias_por_autor = {}
+    Database[SQL[:total_ideias_por_autor]].all.map do |row|
+      @total_ideias_por_autor[row[:autor_id]] = row[:total].to_i
+    end
+    @total_ideias_por_autor
+  end
+
+  # Ideias por data de criação/postagem
+  def total_ideias_por_data_postagem
+    @total_ideias_por_data_postagem || total_ideias_por_data_postagem!
+  end
+
+  def total_ideias_por_data_postagem!
+    @total_ideias_por_data_postagem = {}
+    Database[SQL[:total_ideias_por_data_postagem]].all.map do |row|
+      @total_ideias_por_data_postagem[row[:data_criacao]] = row[:total].to_i
+    end
+    @total_ideias_por_data_postagem
   end
 end
 
