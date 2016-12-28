c6c102d354dc66d936fe2bd7f1faaa0608d43ef7
Hallison Batista <hallison.batista@prodam.am.gov.br>
2016-12-28 15:33:39 -0400

    Consulta das avaliações por situação e data de publicação

--- app/models/avaliacao.rb
+++ app/models/avaliacao.rb
@@ -14,6 +14,15 @@ class Avaliacao < Model[:avaliacao]
 
   many_to_one :ideia
   many_to_one :classificacao
+
+  def self.find_by_situacao_data(situacao, data_inicial, data_final = data_inicial)
+    eager(:ideia, :classificacao).
+    join(:ideia, id: :avaliacao__ideia_id).
+    join(:classificacao, id: :avaliacao__classificacao_id).
+    where("ideia.situacao = ?", situacao.to_s).
+    where("TRUNC(ideia.data_publicacao) >= DATE'#{data_inicial}'").
+    where("TRUNC(ideia.data_publicacao) <= DATE'#{data_final}'")
+  end
 end
 
 end # Prodam::Idealize
