b5240a3eda7280d7b63429a1ce0e12a16e25a1bb
Hallison Batista <hallison.batista@prodam.am.gov.br>
2017-01-31 15:35:02 -0400

    Diversos botões para uma caixa de diálogo

--- public/js/dialog.js
+++ public/js/dialog.js
@@ -1,11 +1,18 @@
 var dialog = document.querySelector('dialog');
-var button = document.querySelector('.mdl-button--open-dialog');
-if (! dialog.showModal) {
+var buttons = document.querySelectorAll('.mdl-button--open-dialog');
+
+if (!dialog.showModal) {
   dialogPolyfill.registerDialog(dialog);
 }
-button.addEventListener('click', function() {
+
+var openDialog = function() {
   dialog.showModal();
-});
+}
+
+for (i = 0; i <= buttons.length - 1; i++) {
+  buttons[i].addEventListener('click', openDialog);
+}
+
 dialog.querySelector('.mdl-button--close-dialog').addEventListener('click', function() {
   dialog.close();
 });
