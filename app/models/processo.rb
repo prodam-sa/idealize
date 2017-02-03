7e737f8d1470cebfec5e6918123ae878f0c068a0
Hallison Batista <hallison.batista@prodam.am.gov.br>
2017-02-03 12:43:42 -0400

    Verificação de processo que bloqueia a ideia

--- app/models/processo.rb
+++ app/models/processo.rb
@@ -5,6 +5,10 @@ module Prodam::Idealize
 class Processo < Model[:processo]
   one_to_one :formulario
   one_to_many :situacoes
+
+  def bloqueia?
+    self[:bloqueia] =~ /s/i && true || false
+  end
 end
 
 end # module
