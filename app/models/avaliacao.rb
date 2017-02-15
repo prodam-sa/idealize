0d116a04d1a6c90ca031008c315bec4507e945de
Hallison Batista <hallison.batista@prodam.am.gov.br>
2017-02-15 15:53:52 -0400

    Correção e melhoria na consulta de avaliações

--- app/models/avaliacao.rb
+++ app/models/avaliacao.rb
@@ -14,10 +14,11 @@ class Avaliacao < Model[:avaliacao]
   many_to_one :classificacao
 
   def self.find_by_situacao_data(situacao, data_inicial, data_final = data_inicial)
-    eager(:ideia, :classificacao).
+    eager(:classificacao, ideia: :situacao).
     join(:ideia, id: :avaliacao__ideia_id).
     join(:classificacao, id: :avaliacao__classificacao_id).
-    where("ideia.situacao = ?", situacao.to_s).
+    join(:situacao, id: :ideia__situacao_id).
+    where("situacao.chave = ?", situacao.to_s).
     where("TRUNC(ideia.data_publicacao) >= DATE'#{data_inicial}'").
     where("TRUNC(ideia.data_publicacao) <= DATE'#{data_final}'")
   end
