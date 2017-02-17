7c4796a1fb46e6a4536ef0e6b54970f93ce088bd
Hallison Batista <hallison.batista@prodam.am.gov.br>
2017-02-17 17:00:58 -0400

    Processo de migração e bootstrap v0.15.0
    - Inclusão dos processos.
    - Inclusão e atualização das situações.
    - Inclusão das premiações.
    - Atualização das classificações e suas premiações.

--- db/v0.15.0/create.sql
+++ db/v0.15.0/create.sql
@@ -4,3 +4,5 @@ define data = &2
 @@db/v0.15.0/create/MENSAGEM.sql &indx &data
 @@db/v0.15.0/create/SITUACAO.sql &indx &data
 @@db/v0.15.0/create/IDEIA.sql &indx &data
+@@db/v0.15.0/create/PREMIACAO.sql &indx &data
+@@db/v0.15.0/create/CLASSIFICACAO_PREMIACAO.sql &indx &data
--- db/v0.15.0/drop.sql
+++ db/v0.15.0/drop.sql
@@ -1,3 +1,5 @@
+@@db/v0.15.0/drop/CLASSIFICACAO_PREMIACAO.sql
+@@db/v0.15.0/drop/PREMIACAO.sql
 @@db/v0.15.0/drop/IDEIA.sql
 @@db/v0.15.0/drop/SITUACAO.sql
 @@db/v0.15.0/drop/MENSAGEM.sql
